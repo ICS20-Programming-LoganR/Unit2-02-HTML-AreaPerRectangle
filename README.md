d37ddc877bd2205aa68caad1be18ce6f73361332
LoganRodriguez2 <99273899+LoganRodriguez@users.noreply.github.com>
2022-03-22 23:09:45 +0000

    all code was changed

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # Unit2-02-HTML-AreaPerRectangle
+
+[![GitHub Super-Linter](https://github.com/nvuillam/npm-groovy-lint/workflows/Lint%20Code%20Base/badge.svg)](https://github.com/marketplace/actions/super-linter)
---
+++ js/script.js
@@ -0,0 +1,11 @@
+//Created by Logan Rodriguez
+//Created on March 2022
+
+function calculateA() {
+  //Calculations for area and perimiter 
+  document.getElementById('multiply-math').innerHTML = 'The area is' + (4 * 18)
+}
+  
+  function calculateP() {
+   document.getElementById('add-math').innerHTML = 'The perimeter is ' + (2 * (4 + 18))
+}
